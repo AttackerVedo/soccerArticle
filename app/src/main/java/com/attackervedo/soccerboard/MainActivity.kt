9d53291a65d73de312be59a589017238a97d4599
AttackerVedo <121778841+AttackerVedo@users.noreply.github.com>
2023-06-26 16:38:10 +0900

    다시시작

---
+++ app/src/main/java/com/attackervedo/soccerboard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.attackervedo.soccerboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
