8e2bb340aef85877a84db9923fd41a46740acb3a
AttackerVedo <121778841+AttackerVedo@users.noreply.github.com>
2023-07-04 22:18:07 +0900

    이준하첫번째프로젝트끝이다.
    포기한부분
    내가쓴댓글 불러오기(불러오기까진했는데 재시작하면 못불러옴)
    그리고댓글 클릭하면 해당글로 가기
    데이터를 어디서끌어와야하는지모르겠음
    아마 디비를 처음부터 이런거까지생각하고짯다면 가능했을수도 이거 할려면 디비를 처음부터 수정해야하고 데이터불러오는거 저장하는거 수정하는거삭제하는거 다바꿔야할수도있어서포기함
    그리고 푸시알림
    그냥 파이어베이스로 푸시알람보내는거나
    버튼눌렀을떄 푸시알람보내는거는 구글링하면서해볼수있겠지만
    댓글이달렸을때 특정유저에서서버에서 보내게하는걸 앱에서코드로 실행을못하겠음
    이거 2개포기했습니다.

--- soccerBoard/app/src/main/java/com/attackervedo/soccerboard/MainActivity.kt
+++ soccerBoard/app/src/main/java/com/attackervedo/soccerboard/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
@@ -13,12 +15,16 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.attackervedo.soccerboard.Activity.IntroActivity
 import com.attackervedo.soccerboard.Activity.MyArticleListActivity
 import com.attackervedo.soccerboard.Activity.MyInfoActivity
+import com.attackervedo.soccerboard.FB.FBRef
+import com.attackervedo.soccerboard.FB.FButils
 import com.attackervedo.soccerboard.Fragment.HomeFragment
 import com.attackervedo.soccerboard.Fragment.NewsFragment
 import com.attackervedo.soccerboard.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
@@ -120,11 +126,51 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
                 val intent = Intent(this@MainActivity, MyArticleListActivity::class.java)
                 startActivity(intent)
             }
+
+            R.id.sideBarUserOut ->{
+                UserOut()
+            }
             R.id.sideBarLogout ->{ logout() }
             else ->{}
         }
         layout_drawer.closeDrawers()
         return false
+    }
+
+    private fun UserOut() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle(Html.fromHtml("<font color='#FF0000'>회원탈퇴 안내문</font>"))
+        dialogBuilder.setMessage(Html.fromHtml("<font color='#FF0000'>정말로 회원탈퇴 하시겠습니까?</font>"))
+
+        dialogBuilder.setPositiveButton("네") { dialog: DialogInterface, which: Int ->
+            // "네" 버튼을 클릭한 경우 처리할 내용
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                currentUser.delete()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // 회원 탈퇴 성공
+                            val uid = currentUser.uid
+                            FBRef.userInfoRef.child(uid).removeValue()
+                            CustomToast.showToast(this, "탈퇴되었습니다.")
+                            finish() // 현재 액티비티 종료
+                        } else {
+                            // 회원 탈퇴 실패
+                            val errorMessage = task.exception?.message
+                            Log.d("errorMessage", errorMessage.toString())
+                            CustomToast.showToast(this, errorMessage.toString())
+                        }
+                    }
+            }
+        }
+
+        dialogBuilder.setNegativeButton("아니요") { dialog: DialogInterface, which: Int ->
+            // "아니요" 버튼을 클릭한 경우 처리할 내용
+            dialog.dismiss()
+        }
+
+        val dialog = dialogBuilder.create()
+        dialog.show()
     }
 
     private fun logout() {
